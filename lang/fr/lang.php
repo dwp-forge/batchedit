be780928d46b44334fa4511ce03493d8ac65aca9
Denis Gravel <dgravel@cegepthetford.ca>
2017-12-09 15:49:05 +0200

    Add French language translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * French language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Denis Gravel <dgravel@cegepthetford.ca>
+ */
+
+// Settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+// For admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu'] = 'Batch Edit';
+
+$lang['err_invreq'] = 'Requête invalide.';
+$lang['err_noregexp'] = 'Expression régulière non précisée.';
+$lang['err_invregexp'] = 'Expression régulière invalide.';
+$lang['err_emptyidx'] = 'L\'index de la page est vide.';
+$lang['err_idxaccess'] = 'Je ne peux avoir accès à l\'index de la page.';
+$lang['err_pregfailed'] = 'Aucune correspondance avec l\'xpression régulière.';
+
+$lang['war_nomatches'] = 'Aucune correspondance trouvée.';
+$lang['war_norights'] = 'Vous n\'avez pas le droit de modifier la page {1}.';
+$lang['war_pagelock'] = 'La page {1} is verrouillée par {2}.';
+
+$lang['btn_preview'] = 'Voir';
+$lang['btn_apply'] = 'Appliquer';
+
+$lang['inf_regexp'] = 'Exemple: /(Wiki)/m';
+$lang['inf_replace'] = 'Exemple: Doku$1';
+
+$lang['lbl_ns'] = 'Espace de noms';
+$lang['lbl_regexp'] = 'Expression régulière';
+$lang['lbl_replace'] = 'Remplacement';
+$lang['lbl_summary'] = 'Sommaire';
+$lang['lbl_minor'] = 'Modifications mineures';
+
+$lang['lnk_view'] = 'Aller à cette page';
+$lang['lnk_edit'] = 'Modifier cette page';
