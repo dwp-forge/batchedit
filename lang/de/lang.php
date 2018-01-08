b41f9921a210735987ef246d444f11ca343f0fa6
Michael Große <grosse@cosmocode.de>
2018-01-08 18:24:34 +0100

    l10n: Use translations closer to the original

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -5,6 +5,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
+
 // settings must be present and set appropriately for the language
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
@@ -14,24 +15,24 @@ $lang['direction']  = 'ltr';
 $lang['menu'] = 'Suchen&Ersetzen';
 
 $lang['err_invreq'] = 'Ungültige Anfrage.';
-$lang['err_noregexp'] = 'Kein Suchbegriff.';
-$lang['err_invregexp'] = 'Ungültiger Suchbegriff.';
+$lang['err_noregexp'] = 'Regulärer Ausduck (Suchbegriff) fehlt';
+$lang['err_invregexp'] = 'Ungültiger Regulärer Ausduck (Suchbegriff)';
 $lang['err_emptyidx'] = 'Seitenindex ist leer.';
 $lang['err_idxaccess'] = 'Seitenindex kann nicht geladen werden.';
-$lang['err_pregfailed'] = 'Suchbegriff fehlerhaft.';
+$lang['err_pregfailed'] = 'Suche gescheitert.';
 
 $lang['war_nomatches'] = 'Keine Suchergebnisse gefunden.';
 $lang['war_norights'] = 'Keine Berechtigungen um Seite {1} zu bearbeiten.';
 $lang['war_pagelock'] = 'Seite {1} wird gerade bearbeitet von {2}.';
 
 $lang['btn_preview'] = 'Vorschau';
-$lang['btn_apply'] = 'OK';
+$lang['btn_apply'] = 'Anwenden';
 
 $lang['inf_regexp'] = 'Beispiel: /(Wiki)/m';
 $lang['inf_replace'] = 'Beispiel: Doku$1';
 
-$lang['lbl_ns'] = 'Namespace';
-$lang['lbl_regexp'] = 'Suchbegriff (/suche/)';
+$lang['lbl_ns'] = 'Namensraum';
+$lang['lbl_regexp'] = 'Regulärer Ausdruck (/suche/)';
 $lang['lbl_replace'] = 'Ersetzen durch';
 $lang['lbl_summary'] = 'Zusammenfassung';
 $lang['lbl_minor'] = 'Kleine Änderung';
