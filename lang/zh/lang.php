26e780c5827b4b9996ff6917b1e982d92cb7e0a4
Luminisward <luminis@vip.qq.com>
2018-04-29 03:15:00 +0800

    Add Chinese(Simplified) translation

---
+++ lang/zh/lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Chinese(Simplified) language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     luminisward <luminis@vip.qq.com>
+ */
+
+// Settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+
+// For admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu'] = '批量修改';
+
+$lang['err_invreq'] = '无效请求。';
+$lang['err_noregexp'] = '未指定正则表达式。';
+$lang['err_invregexp'] = '无效的正则表达式。';
+$lang['err_emptyidx'] = '页面索引为空。';
+$lang['err_idxaccess'] = '无法访问页面索引。';
+$lang['err_pregfailed'] = '正则表达式错误。';
+
+$lang['war_nomatches'] = '找不到匹配文本。';
+$lang['war_norights'] = '您没有权限编辑页面 {1}。';
+$lang['war_pagelock'] = '页面 {1} 被用户 {2} 锁定。';
+
+$lang['btn_preview'] = '预览';
+$lang['btn_apply'] = '执行';
+
+$lang['inf_regexp'] = '举例: /(Wiki)/m';
+$lang['inf_replace'] = '举例: Doku$1';
+
+$lang['lbl_ns'] = '命名空间';
+$lang['lbl_regexp'] = '正则表达式';
+$lang['lbl_replace'] = '替换';
+$lang['lbl_summary'] = '编辑摘要';
+$lang['lbl_minor'] = '细微修改';
+
+$lang['lnk_view'] = '前往此页';
+$lang['lnk_edit'] = '修改此页';
