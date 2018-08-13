b8a505246ddb064628acb20540cec7755e4f2166
Michael Große <grosse@cosmocode.de>
2018-08-14 00:06:41 +0300

    Update German translation

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,37 +1,58 @@
 <?php
+
 /**
  * Plugin BatchEdit: German language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Michael Große <grosse@cosmocode.de>
  */
-
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'Suchen&Ersetzen';
-
-$lang['err_invreq'] = 'Ungültige Anfrage.';
-$lang['err_nosearch'] = 'Suchbegriff fehlt.';
-$lang['err_invregexp'] = 'Ungültiger Regulärer Ausduck (Suchbegriff)';
-$lang['err_emptyidx'] = 'Seitenindex ist leer.';
-$lang['err_idxaccess'] = 'Seitenindex kann nicht geladen werden.';
-$lang['err_pregfailed'] = 'Suche gescheitert.';
-
-$lang['war_nomatches'] = 'Keine Suchergebnisse gefunden.';
-$lang['war_norights'] = 'Keine Berechtigungen um Seite {1} zu bearbeiten.';
-$lang['war_pagelock'] = 'Seite {1} wird gerade bearbeitet von {2}.';
-
-$lang['btn_preview'] = 'Vorschau';
-$lang['btn_apply'] = 'Anwenden';
-
-$lang['lbl_ns'] = 'Namensraum';
-$lang['lbl_replace'] = 'Ersetzen durch';
-$lang['lbl_summary'] = 'Zusammenfassung';
-$lang['lbl_minor'] = 'Kleine Änderung';
-$lang['lbl_searchregexp'] = 'Regulärer Ausdruck';
-
-$lang['ttl_view'] = 'Zu dieser Seite';
-$lang['ttl_edit'] = 'Seite bearbeiten';
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'Suchen&Ersetzen';
+$lang['err_invreq']            = 'Ungültige Anfrage.';
+$lang['err_nosearch']          = 'Suchbegriff fehlt.';
+$lang['err_invregexp']         = 'Ungültiger Regulärer Ausduck (Suchbegriff)';
+$lang['err_emptyidx']          = 'Seitenindex ist leer.';
+$lang['err_idxaccess']         = 'Seitenindex kann nicht geladen werden.';
+$lang['err_pregfailed']        = 'Suche gescheitert.';
+$lang['war_nomatches']         = 'Keine Suchergebnisse gefunden.';
+$lang['war_norights']          = 'Keine Berechtigungen um Seite {1} zu bearbeiten.';
+$lang['war_pagelock']          = 'Seite {1} wird gerade bearbeitet von {2}.';
+$lang['war_searchlimit']       = 'Die Suche wurde unterbrochen, nachdem die maximale Anzahl an Treffen gefunden wurde.';
+$lang['war_timeout']           = 'Die Operation wurde unterbrochen, weil sie zu lange dauert.';
+$lang['war_cancelled']         = 'Der Vorgang wurde auf Wunsch des Benutzers unterbrochen.';
+$lang['war_nosummary']         = 'Die Bearbeitungszusammenfassung wurde nicht zur Verfügung gestellt. Wollen Sie ohne sie weitermachen?';
+$lang['msg_error']             = '<b>Fehler:</b> {1}';
+$lang['msg_warning']           = '<b>Warnung:</b> {1}';
+$lang['btn_preview']           = 'Vorschau';
+$lang['btn_apply']             = 'Anwenden';
+$lang['btn_cancel']            = 'Abbrechen';
+$lang['hnt_textsearch']        = 'AnyWiki';
+$lang['hnt_textreplace']       = 'DokuWiki';
+$lang['hnt_regexpsearch']      = '\w+(Wiki)';
+$lang['hnt_regexpreplace']     = 'Doku$1';
+$lang['hnt_advregexpsearch']   = '/\w+(Wiki)/m';
+$lang['lbl_ns']                = 'Namensraum';
+$lang['lbl_search']            = 'Suche nach';
+$lang['lbl_replace']           = 'Ersetzen durch';
+$lang['lbl_summary']           = 'Zusammenfassung';
+$lang['lbl_minor']             = 'Kleine Änderung';
+$lang['lbl_searchmode']        = 'Suchmodus';
+$lang['lbl_searchtext']        = 'EInfacher Text';
+$lang['lbl_searchregexp']      = 'Regulärer Ausdruck';
+$lang['lbl_matchcase']         = 'Groß-/Kleinschreibung beachten';
+$lang['lbl_multiline']         = 'Mehrzeilig';
+$lang['lbl_advregexp']         = 'Verwende Trennzeichen und Modifikatoren in regulären Ausdrücken';
+$lang['sts_preview']           = 'Suchergebnisse: {1} auf {2}';
+$lang['sts_matches#one']       = 'Treffer';
+$lang['sts_matches#many']      = 'Treffer';
+$lang['sts_pages#one']         = 'Seite';
+$lang['sts_pages#many']        = 'Seiten';
+$lang['ttl_applyall']          = 'Alle Ersetzungen ausführen';
+$lang['ttl_applyfile']         = 'Alle Ersetzungen in dieser Datei ausführen';
+$lang['ttl_applymatch']        = 'Diese Ersetzung ausführen';
+$lang['ttl_view']              = 'Zu dieser Seite';
+$lang['ttl_edit']              = 'Seite bearbeiten';
+$lang['ttl_mainform']          = 'Gehe nach unten';
+$lang['ttl_extoptions']        = 'Erweiterte Optionen';
---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Michael Große <grosse@cosmocode.de>
+ */
+$lang['timelimit']             = 'Zeitlimit für die Ausführung der Plugin-Operationen (in Sekunden, 0 für das PHP default limit)';
+$lang['sessionexp']            = 'Ablaufzeit der Sitzung (in Sekunden)';
