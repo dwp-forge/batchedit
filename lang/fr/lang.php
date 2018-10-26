46321c9dbd4f7447cbcaf139f5d149d917021bc6
Schplurtz le Déboulonné <schplurtz@laposte.net>
2018-10-26 05:05:36 +0200

    Update Franch translation

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,37 +1,75 @@
 <?php
+
 /**
  * Plugin BatchEdit: French language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Denis Gravel <dgravel@cegepthetford.ca>
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ * @author Denis Gravel <dgravel@cegepthetford.ca>
  */
-
-// Settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-// For admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'BatchEdit';
-
-$lang['err_invreq'] = 'Requête invalide.';
-$lang['err_invregexp'] = 'Expression régulière invalide.';
-$lang['err_emptyidx'] = 'L\'index de la page est vide.';
-$lang['err_idxaccess'] = 'Je ne peux avoir accès à l\'index de la page.';
-$lang['err_pregfailed'] = 'Aucune correspondance avec l\'xpression régulière.';
-
-$lang['war_nomatches'] = 'Aucune correspondance trouvée.';
-$lang['war_norights'] = 'Vous n\'avez pas le droit de modifier la page {1}.';
-$lang['war_pagelock'] = 'La page {1} is verrouillée par {2}.';
-
-$lang['btn_preview'] = 'Voir';
-$lang['btn_apply'] = 'Appliquer';
-
-$lang['lbl_ns'] = 'Espace de noms';
-$lang['lbl_replace'] = 'Remplacement';
-$lang['lbl_summary'] = 'Sommaire';
-$lang['lbl_minor'] = 'Modifications mineures';
-$lang['lbl_searchregexp'] = 'Expression régulière';
-
-$lang['ttl_view'] = 'Aller à cette page';
-$lang['ttl_edit'] = 'Modifier cette page';
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'BatchEdit';
+$lang['err_invreq']            = 'Requête invalide.';
+$lang['err_nosearch']          = 'Expression de recherche non précisée.';
+$lang['err_invregexp']         = 'Expression régulière invalide.';
+$lang['err_emptyidx']          = 'L\'index de page est vide.';
+$lang['err_idxaccess']         = 'Ne peut accéder à l\'index de page.';
+$lang['err_emptyns']           = 'Les pages trouvées dans la catégorie "{1}"';
+$lang['err_pregfailed']        = 'Aucune correspondance avec l\'expression régulière.';
+$lang['war_nomatches']         = 'Aucune correspondance trouvée.';
+$lang['war_norights']          = 'Vous n\'avez pas le droit de modifier la page {1}.';
+$lang['war_pagelock']          = '{2} a verrouillé la page {1}.';
+$lang['war_matchfail']         = 'Échec dans le remplacement de l’occurrence {1}.';
+$lang['war_searchlimit']       = 'La recherche s\'est interrompue après avoir atteint le nombre maximal de correspondances.';
+$lang['war_timeout']           = 'L\'opération a été interrompue car elle prend trop de temps.';
+$lang['war_cancelled']         = 'L\'opération a été interrompue  à la demande de l\'utilisateur.';
+$lang['war_nosummary']         = 'Le résumé des changements n\'est pas disponible. Voulez vous continuer sans ?';
+$lang['msg_error']             = '<b>Erreur :</b> {1}';
+$lang['msg_warning']           = '<b>Attention :</b> {1}';
+$lang['btn_preview']           = 'Voir';
+$lang['btn_apply']             = 'Appliquer';
+$lang['btn_cancel']            = 'Annuler';
+$lang['hnt_textsearch']        = 'ToutWiki';
+$lang['hnt_textreplace']       = 'DokuWiki';
+$lang['hnt_regexpsearch']      = '\w+(Wiki)';
+$lang['hnt_regexpreplace']     = 'Doku$1';
+$lang['hnt_advregexpsearch']   = '/\w+(Wiki)/m';
+$lang['lbl_ns']                = 'Catégorie';
+$lang['lbl_search']            = 'Rechercher';
+$lang['lbl_replace']           = 'Remplacement';
+$lang['lbl_summary']           = 'Résumé des changements';
+$lang['lbl_minor']             = 'Modifications mineures';
+$lang['lbl_searchmode']        = 'Mode de recherche';
+$lang['lbl_searchtext']        = 'Plein texte';
+$lang['lbl_searchregexp']      = 'Expression régulière';
+$lang['lbl_matchcase']         = 'Sensible à la casse';
+$lang['lbl_multiline']         = 'Multi-ligne';
+$lang['lbl_advregexp']         = 'Utiliser les délimiteurs et modificateurs dans les expressions régulières';
+$lang['lbl_matchctx']          = 'Montrer le contexte des résultats des  {1} caractères or {2} lignes';
+$lang['lbl_searchlimit']       = 'Arrêter la recherche après avoir trouvé les  {1} premiers résultats';
+$lang['lbl_keepmarks2']        = 'Même occurrence';
+$lang['lbl_keepmarks3']        = 'Même décalage';
+$lang['lbl_keepmarks4']        = 'J\'ai de la chance !';
+$lang['lbl_checksummary']      = 'Demander confirmation pour appliquer les changements sans résumé';
+$lang['lbl_searching']         = 'Recherche...';
+$lang['lbl_applying']          = 'Remplacements...';
+$lang['sts_preview']           = 'Résultats de recherche : {1} sur {2}';
+$lang['sts_apply']             = 'Résultat d\'édition : {1} sur {2}, {3}';
+$lang['sts_page']              = '{1} &ndash; {2}';
+$lang['sts_matches#one']       = '{1} occurrence';
+$lang['sts_matches#many']      = '{1} occurrences';
+$lang['sts_pages#one']         = '{1} page';
+$lang['sts_pages#many']        = '{1} pages';
+$lang['sts_edits#one']         = '{1} replacement appliqué';
+$lang['sts_edits#many']        = '{1} replacements appliqués';
+$lang['ttl_applyall']          = 'Appliquer toutes les occurrences';
+$lang['ttl_applyfile']         = 'Appliquer toutes les occurrences dans ce fichier';
+$lang['ttl_applymatch']        = 'Appliquer cette occurrence';
+$lang['ttl_view']              = 'Aller à cette page';
+$lang['ttl_edit']              = 'Modifier cette page';
+$lang['ttl_mainform']          = 'Aller en bas';
+$lang['ttl_extoptions']        = 'Options avancées';
+$lang['dim_options']           = '13em';
+$lang['dim_extoptions']        = '28em';
