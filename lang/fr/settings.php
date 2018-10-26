baa39e722bdb206752cc1d34f5f16ad2653d48fd
Schplurtz le Déboulonné <schplurtz@laposte.net>
2018-10-26 05:05:36 +0200

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['timelimit']             = 'Délai d\'exécution des opérations en secondes. (0 = valeur par défaut de PHP)';
+$lang['sessionexp']            = 'Délai d\'expiration de la session (secondes)';
