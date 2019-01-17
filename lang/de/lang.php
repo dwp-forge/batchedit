3bf8decca121d512f325b615b7f922236b6f14bf
Samana Johann <samana.johann@sangham.net>
2019-01-17 17:25:28 +0100

    Update German translation

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -5,6 +5,7 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Samana Johann <samana.johann@sangham.net>
  * @author Michael Große <grosse@cosmocode.de>
  */
 $lang['encoding']              = 'utf-8';
@@ -15,10 +16,12 @@ $lang['err_nosearch']          = 'Suchbegriff fehlt.';
 $lang['err_invregexp']         = 'Ungültiger Regulärer Ausduck (Suchbegriff)';
 $lang['err_emptyidx']          = 'Seitenindex ist leer.';
 $lang['err_idxaccess']         = 'Seitenindex kann nicht geladen werden.';
-$lang['err_pregfailed']        = 'Suche gescheitert.';
+$lang['err_emptyns']           = 'Seiten, die im Namensraum "{1}" gefunden wurden.';
+$lang['err_pregfailed']        = 'Suche (regex) gescheitert.';
 $lang['war_nomatches']         = 'Keine Suchergebnisse gefunden.';
 $lang['war_norights']          = 'Keine Berechtigungen um Seite {1} zu bearbeiten.';
 $lang['war_pagelock']          = 'Seite {1} wird gerade bearbeitet von {2}.';
+$lang['war_matchfail']         = 'Fund für {1} auszuführen scheitete.';
 $lang['war_searchlimit']       = 'Die Suche wurde unterbrochen, nachdem die maximale Anzahl an Treffen gefunden wurde.';
 $lang['war_timeout']           = 'Die Operation wurde unterbrochen, weil sie zu lange dauert.';
 $lang['war_cancelled']         = 'Der Vorgang wurde auf Wunsch des Benutzers unterbrochen.';
@@ -44,11 +47,24 @@ $lang['lbl_searchregexp']      = 'Regulärer Ausdruck';
 $lang['lbl_matchcase']         = 'Groß-/Kleinschreibung beachten';
 $lang['lbl_multiline']         = 'Mehrzeilig';
 $lang['lbl_advregexp']         = 'Verwende Trennzeichen und Modifikatoren in regulären Ausdrücken';
+$lang['lbl_matchctx']          = 'Zeige Fundinhalt im Umfang von {1} Zeichen {2} Zeilen.';
+$lang['lbl_searchlimit']       = 'Beende Suche nach Finden von {1} Übereinstimmungen';
+$lang['lbl_keepmarks']         = 'Erhalte markierte Funde in der Voransicht {1}';
+$lang['lbl_keepmarks1']        = 'Sicherer Modus';
+$lang['lbl_keepmarks2']        = 'Gleicher Fund';
+$lang['lbl_keepmarks4']        = 'Ich bin erfreut!';
+$lang['lbl_checksummary']      = 'Zeige Bestättigung der Durchführung von Änderungen ohne Zusammenfassung';
+$lang['lbl_searching']         = 'Suche...';
+$lang['lbl_applying']          = 'Wird durchgeführt...';
 $lang['sts_preview']           = 'Suchergebnisse: {1} auf {2}';
+$lang['sts_apply']             = 'Bearbeite Ergebnisse: {1} mit {2}, {3}';
+$lang['sts_page']              = '{1} &ndash; {2}';
 $lang['sts_matches#one']       = 'Treffer';
 $lang['sts_matches#many']      = 'Treffer';
 $lang['sts_pages#one']         = 'Seite';
 $lang['sts_pages#many']        = 'Seiten';
+$lang['sts_edits#one']         = '{1} Ersetzung durchgeführt.';
+$lang['sts_edits#many']        = '{1} Ersetzungen durchgeführt.';
 $lang['ttl_applyall']          = 'Alle Ersetzungen ausführen';
 $lang['ttl_applyfile']         = 'Alle Ersetzungen in dieser Datei ausführen';
 $lang['ttl_applymatch']        = 'Diese Ersetzung ausführen';
@@ -56,3 +72,5 @@ $lang['ttl_view']              = 'Zu dieser Seite';
 $lang['ttl_edit']              = 'Seite bearbeiten';
 $lang['ttl_mainform']          = 'Gehe nach unten';
 $lang['ttl_extoptions']        = 'Erweiterte Optionen';
+$lang['dim_options']           = '13em';
+$lang['dim_extoptions']        = '28em';
