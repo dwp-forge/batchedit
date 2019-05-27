aaaf95230ca8b339091a5ea20231d4b00cc02cd3
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-05-28 08:38:45 +1000

    Add Russian localization

---
+++ lang/ru/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin BatchEdit: Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+ 
+$lang['timelimit'] = 'Ограничение по времени выполнения операций плагина (секунды, 0 - PHP по умолчанию)';
+$lang['sessionexp'] = 'Время окончания сеанса (в секундах)';
