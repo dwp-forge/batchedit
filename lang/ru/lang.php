0632e5673a38faf5ae31da11d72ab9c02378c7e8
Aleksandr Selivanov <alexgearbox@yandex.ru>
2021-02-05 20:25:11 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,88 +1,77 @@
 <?php
+
 /**
  * Plugin BatchEdit: Russian language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
-
-// Settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-// For admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'BatchEdit';
-
-$lang['err_invreq'] = 'Неверный запрос.';
-$lang['err_nosearch'] = 'Поисковое выражение не указано.';
-$lang['err_invregexp'] = 'Неверное регулярное выражение.';
-$lang['err_emptyidx'] = 'Индекс страницы пуст.';
-$lang['err_idxaccess'] = 'Индекс страницы недоступен.';
-$lang['err_emptyns'] = 'В папке "{1}" найдены страницы.';
-$lang['err_pregfailed'] = 'Ошибка сопоставления регулярного выражения.';
-
-$lang['war_nomatches'] = 'Совпадений не найдено.';
-$lang['war_norights'] = 'У вас нет прав на редактирование страницы {1}.';
-$lang['war_pagelock'] = 'Страница {1} занята {2}.';
-$lang['war_matchfail'] = 'Не удалось применить совпадение {1}.';
-$lang['war_searchlimit'] = 'Поиск был прерван после достижения максимального количества совпадений.';
-$lang['war_timeout'] = 'Операция была прервана из-за превышения лимита времени.';
-$lang['war_cancelled'] = 'Операция была прервана по запросу пользователя.';
-$lang['war_nosummary'] = 'Сводка изменений не заполнена. Вы хотите продолжить без неё?';
-
-$lang['msg_error'] = '<b>Ошибка:</b> {1}';
-$lang['msg_warning'] = '<b>Внимание:</b> {1}';
-
-$lang['btn_preview'] = 'Предпросмотр';
-$lang['btn_apply'] = 'Применить';
-$lang['btn_cancel'] = 'Отменить';
-
-$lang['hnt_textsearch'] = 'AnyWiki';
-$lang['hnt_textreplace'] = 'DokuWiki';
-$lang['hnt_regexpsearch'] = '\w+(Wiki)';
-$lang['hnt_regexpreplace'] = 'Doku$1';
-$lang['hnt_advregexpsearch'] = '/\w+(Wiki)/m';
-
-$lang['lbl_ns'] = 'Папка';
-$lang['lbl_search'] = 'Искать';
-$lang['lbl_replace'] = 'Заменить';
-$lang['lbl_summary'] = 'Сводка изменений';
-$lang['lbl_minor'] = 'Незначительные изменения';
-$lang['lbl_searchmode'] = 'Режим поиска';
-$lang['lbl_searchtext'] = 'Простой текст';
-$lang['lbl_searchregexp'] = 'Регулярное выражение';
-$lang['lbl_matchcase'] = 'Зависит от регистра';
-$lang['lbl_multiline'] = 'Многострочный';
-$lang['lbl_advregexp'] = 'Использовать в регулярке делимитеры и моификаторы';
-$lang['lbl_matchctx'] = 'Показать контекст соответствия символов {1} или строк {2}';
-$lang['lbl_searchlimit'] = 'Остановить поиск после нахождения первых {1} совпадений';
-$lang['lbl_keepmarks'] = 'Сохранить отмеченные совпадения при предпросмотре {1}';
-$lang['lbl_keepmarks1'] = 'Безопасный режим';
-$lang['lbl_keepmarks2'] = 'То же совпадение';
-$lang['lbl_keepmarks3'] = 'То же смещение';
-$lang['lbl_keepmarks4'] = 'Я чую удачу!';
-$lang['lbl_checksummary'] = 'Показsdать подтверждение о применении правок без сводки';
-$lang['lbl_searching'] = 'Поиск...';
-$lang['lbl_applying'] = 'Применение...';
-
-$lang['sts_preview'] = 'Результаты поиска: {1} в {2}';
-$lang['sts_apply'] = 'Результаты правок: {1} в {2}, {3}';
-$lang['sts_page'] = '{1} &ndash; {2}';
-$lang['sts_matches#one'] = '{1} совпадение';
-$lang['sts_matches#many'] = '{1} совпадений';
-$lang['sts_pages#one'] = '{1} страница';
-$lang['sts_pages#many'] = '{1} страниц';
-$lang['sts_edits#one'] = '{1} замена применена';
-$lang['sts_edits#many'] = '{1} замен применено';
-
-$lang['ttl_applyall'] = 'Применить все совпадения';
-$lang['ttl_applyfile'] = 'Применить все совпадения в этом файле';
-$lang['ttl_applymatch'] = 'Применить это совпадение';
-$lang['ttl_view'] = 'Перейти на эту страницу';
-$lang['ttl_edit'] = 'Править эту страницу';
-$lang['ttl_mainform'] = 'Перейти вниз';
-$lang['ttl_extoptions'] = 'Расширенные опции';
-
-$lang['dim_options'] = '13em';
-$lang['dim_extoptions'] = '28em';
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'BatchEdit';
+$lang['err_invreq']            = 'Неверный запрос.';
+$lang['err_nosearch']          = 'Поисковое выражение не указано.';
+$lang['err_invregexp']         = 'Неверное регулярное выражение.';
+$lang['err_emptyidx']          = 'Индекс страницы пуст.';
+$lang['err_idxaccess']         = 'Индекс страницы недоступен.';
+$lang['err_emptyns']           = 'В папке "{1}" найдены страницы.';
+$lang['err_pregfailed']        = 'Ошибка сопоставления регулярного выражения.';
+$lang['war_nomatches']         = 'Совпадений не найдено.';
+$lang['war_norights']          = 'У вас нет прав на редактирование страницы {1}.';
+$lang['war_pagelock']          = 'Страница {1} занята {2}.';
+$lang['war_matchfail']         = 'Не удалось применить совпадение {1}.';
+$lang['war_searchlimit']       = 'Поиск был прерван после достижения максимального количества совпадений.';
+$lang['war_timeout']           = 'Операция была прервана из-за превышения лимита времени.';
+$lang['war_cancelled']         = 'Операция была прервана по запросу пользователя.';
+$lang['war_nosummary']         = 'Сводка изменений не заполнена. Вы хотите продолжить без неё?';
+$lang['msg_error']             = '<b>Ошибка:</b> {1}';
+$lang['msg_warning']           = '<b>Внимание:</b> {1}';
+$lang['btn_preview']           = 'Предпросмотр';
+$lang['btn_apply']             = 'Применить';
+$lang['btn_cancel']            = 'Отменить';
+$lang['hnt_textsearch']        = 'AnyWiki';
+$lang['hnt_textreplace']       = 'DokuWiki';
+$lang['hnt_regexpsearch']      = '\w+(Wiki)';
+$lang['hnt_regexpreplace']     = 'Doku$1';
+$lang['hnt_advregexpsearch']   = '/\w+(Wiki)/m';
+$lang['lbl_ns']                = 'Пространство имён';
+$lang['lbl_search']            = 'Искать';
+$lang['lbl_replace']           = 'Заменить на';
+$lang['lbl_summary']           = 'Сводка изменений';
+$lang['lbl_minor']             = 'Незначительные изменения';
+$lang['lbl_searchmode']        = 'Режим поиска';
+$lang['lbl_searchtext']        = 'Простой текст';
+$lang['lbl_searchregexp']      = 'Регулярное выражение';
+$lang['lbl_matchcase']         = 'С учётом регистра';
+$lang['lbl_multiline']         = 'Многострочный';
+$lang['lbl_advregexp']         = 'Использовать в регулярном выражении разделители и модификаторы';
+$lang['lbl_matchctx']          = 'Показать контекст соответствия символов {1} или строк {2}';
+$lang['lbl_searchlimit']       = 'Остановить поиск после нахождения первых {1} совпадений';
+$lang['lbl_keepmarks']         = 'Сохранить отмеченные совпадения при предпросмотре {1}';
+$lang['lbl_keepmarks1']        = 'Безопасный режим';
+$lang['lbl_keepmarks2']        = 'То же совпадение';
+$lang['lbl_keepmarks3']        = 'То же смещение';
+$lang['lbl_keepmarks4']        = 'Я чую удачу!';
+$lang['lbl_checksummary']      = 'Показsdать подтверждение о применении правок без сводки';
+$lang['lbl_searching']         = 'Поиск...';
+$lang['lbl_applying']          = 'Применение...';
+$lang['sts_preview']           = 'Результаты поиска: {1} в {2}';
+$lang['sts_apply']             = 'Результаты правок: {1} в {2}, {3}';
+$lang['sts_page']              = '{1} &ndash; {2}';
+$lang['sts_matches#one']       = '{1} совпадение';
+$lang['sts_matches#many']      = '{1} совпадений';
+$lang['sts_pages#one']         = '{1} страница';
+$lang['sts_pages#many']        = '{1} страниц';
+$lang['sts_edits#one']         = '{1} замена применена';
+$lang['sts_edits#many']        = '{1} замен применено';
+$lang['ttl_applyall']          = 'Применить все совпадения';
+$lang['ttl_applyfile']         = 'Применить все совпадения в этом файле';
+$lang['ttl_applymatch']        = 'Применить это совпадение';
+$lang['ttl_view']              = 'Перейти на эту страницу';
+$lang['ttl_edit']              = 'Править эту страницу';
+$lang['ttl_mainform']          = 'Перейти вниз';
+$lang['ttl_extoptions']        = 'Дополнительные настройки';
+$lang['dim_options']           = '13em';
+$lang['dim_extoptions']        = '28em';
