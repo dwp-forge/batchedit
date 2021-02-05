c5e886dd1237540347bf971f6cdfc74473897945
Aleksandr Selivanov <alexgearbox@yandex.ru>
2021-02-05 20:25:11 +0100

    Update Russian localization

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -4,8 +4,9 @@
  * Plugin BatchEdit: Russian language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Rainbow-Spike <rainbow_spike@derpy.ru>
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
- 
-$lang['timelimit'] = 'Ограничение по времени выполнения операций плагина (секунды, 0 - PHP по умолчанию)';
-$lang['sessionexp'] = 'Время окончания сеанса (в секундах)';
+$lang['timelimit']             = 'Ограничение по времени выполнения операций плагина (в секундах, 0 - PHP по умолчанию)';
+$lang['sessionexp']            = 'Время окончания сеанса (в секундах)';
