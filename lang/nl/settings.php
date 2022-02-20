e7942d8ef8862e8646adc1fe682cc8e32c18557d
Gerrit <klapinklapin@gmail.com>
2022-02-20 15:45:09 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Gerrit <klapinklapin@gmail.com>
+ */
+$lang['timelimit']             = 'Tijdslimiet voor uitvoeren van opdrachten (seconden, 0 = Standaard tijd van PHP)';
+$lang['sessionexp']            = 'Sessie verlooptijd (seconden)';
