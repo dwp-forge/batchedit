11855720010f7935d3053b512a56457c272201a9
Gerrit <klapinklapin@gmail.com>
2022-02-20 15:45:09 +0100

    Add Dutch localization

---
+++ lang/nl/lang.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Gerrit <klapinklapin@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'BatchEdit';
+$lang['err_invreq']            = 'Ongeldig verzoek.';
+$lang['err_nosearch']          = 'Zoekopdracht is niet gespecificeerd.';
+$lang['err_invregexp']         = 'Ongeldige reguliere expressie';
+$lang['err_emptyidx']          = 'De pagina-index is leeg.';
+$lang['err_idxaccess']         = 'Geen toegang tot de pagina-index.';
+$lang['err_emptyns']           = 'Geen pagina\'s gevonden in namespace "{1}".';
+$lang['err_pregfailed']        = 'Geen resultaten gevonden voor deze Reguliere expressie.';
+$lang['war_nomatches']         = 'Geen resultaten gevonden.';
+$lang['war_norights']          = 'Je hebt geen rechten om de pagina te bewerken {1}.';
+$lang['war_pagelock']          = 'Pagina {1} is vergrendeld door {2}.';
+$lang['war_matchfail']         = 'Geen overeenkomst gevonden {1}.';
+$lang['war_searchlimit']       = 'De zoekopdracht was onderbroken na het bereiken van het maximum aantal resultaten.';
+$lang['war_timeout']           = 'De opdracht was onderbroken omdat die te veel tijd kost.';
+$lang['war_cancelled']         = 'De opdracht was onderbroken op verzoek van de gebruiker.';
+$lang['war_nosummary']         = 'De bewerksamenvatting was niet gegeven. Wil je door gaan zonder samenvatting in te vullen?';
+$lang['msg_error']             = '<b>Fout:</b> {1}';
+$lang['msg_warning']           = '<b>Waarschuwing:</b> {1}';
+$lang['btn_preview']           = 'Voorbeeld';
+$lang['btn_apply']             = 'Toepassen';
+$lang['btn_cancel']            = 'Annuleren';
+$lang['hnt_textsearch']        = 'AnyWiki';
+$lang['hnt_textreplace']       = 'DokuWiki';
+$lang['hnt_regexpsearch']      = '\w+(Wiki)';
+$lang['hnt_regexpreplace']     = 'Doku$1';
+$lang['hnt_advregexpsearch']   = '/\w+(Wiki)/m';
+$lang['lbl_ns']                = 'Namespace';
+$lang['lbl_search']            = 'Zoek voor';
+$lang['lbl_replace']           = 'Vervang met';
+$lang['lbl_summary']           = 'Bewerksamenvatting';
+$lang['lbl_minor']             = 'Kleine aanpassing';
+$lang['lbl_searchmode']        = 'Zoekmodus';
+$lang['lbl_searchtext']        = 'Platte tekst';
+$lang['lbl_searchregexp']      = 'Reguliere expressie';
+$lang['lbl_matchcase']         = 'Hoofdlettergevoelig';
+$lang['lbl_multiline']         = 'Meerdere regels';
+$lang['lbl_advregexp']         = 'Gebruik scheidingstekens (delimiters) en modifiers in de reguliere expressie';
+$lang['lbl_matchctx']          = 'Laat resultaat context zien van {1} tekens of {2} regels';
+$lang['lbl_searchlimit']       = 'Stop zoeken na vinden van de eerste {1} resultaten';
+$lang['lbl_keepmarks']         = 'Bewaar gemarkeerde resultaten on preview {1}';
+$lang['lbl_keepmarks1']        = 'Veilige modus';
+$lang['lbl_keepmarks2']        = 'Zelfde resultaat';
+$lang['lbl_keepmarks3']        = 'Zelfde offset';
+$lang['lbl_keepmarks4']        = 'I feel lucky!';
+$lang['lbl_tplpatterns']       = 'Pas namespace-template-vervangingen toe';
+$lang['lbl_checksummary']      = 'Vraag bevestiging bij bewerkingen zonder een bewerksamenvatting';
+$lang['lbl_searching']         = 'Zoeken...';
+$lang['lbl_applying']          = 'Toepassen...';
+$lang['sts_preview']           = 'Zoekresultaten {1} tot {2}';
+$lang['sts_apply']             = 'Bewerkresultaten {1} tot {2}, {3}';
+$lang['sts_page']              = '{1} &ndash; {2}';
+$lang['sts_matches#one']       = '{1} resultaat';
+$lang['sts_matches#many']      = '{2} resultaten';
+$lang['sts_pages#one']         = '{1} pagina';
+$lang['sts_pages#many']        = '{1} pagina\'s';
+$lang['sts_edits#one']         = '{1} vervanging gedaan';
+$lang['sts_edits#many']        = '{1} vervangingen gedaan';
+$lang['ttl_applyall']          = 'Alle resultaten uitvoeren';
+$lang['ttl_applyfile']         = 'Alle resultaten in dit bestand toepassen';
+$lang['ttl_applymatch']        = 'Dit resultaat toepassen';
+$lang['ttl_view']              = 'Ga naar deze pagina';
+$lang['ttl_edit']              = 'Bewerk deze pagina';
+$lang['ttl_mainform']          = 'Ga naar onderen';
+$lang['ttl_extoptions']        = 'Geavanceerde opties';
+$lang['dim_options']           = '13em';
+$lang['dim_extoptions']        = '36em';
